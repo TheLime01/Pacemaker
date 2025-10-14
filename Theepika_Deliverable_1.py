5090ea755ec4fcb60a75e178ee4a22a7881cd5aa
pirament <pirament@mcmaster.ca>
2025-10-14 15:35:17 -0400

    Update Theepika_Deliverable_1.py

--- Theepika_Deliverable_1.py
+++ Theepika_Deliverable_1.py
@@ -16,7 +16,7 @@ Ventricular_Pulse_Width = [0.05,0.4,1.9,0.4,0.4] #ms
 VRP = [150,320,500,320,320] #ms
 ARP = [150,250,500,250,250] #ms
 
-# Const 
+# Constants
 Modes = ["Off", "AOO","VOO", "AAI", "VVI"]
 Programable_Paramters = ["Lower Rate Limit", "Upper Rate Limit", "Atrial Amplitude","Atrial Pules Width",
                          "Ventricular Amplitude", "Ventricular Pulse Width", "VRP", "ARP"]
@@ -27,11 +27,13 @@ Mode_Parameters = [[False,False,False,False,False,False,False,False],
                    [True,True,False,False,True,True,True,False]]
 
 
-
-
-
-
-
+mode_parameters = {
+    "Off": [" "],
+    "AOO": ["Lower_Rate_Limit", "Upper_Rate_Limit", "Atrial_Amplitude", "Atrial_Pules_Width"],
+    "VOO": ["Lower_Rate_Limit", "Upper_Rate_Limit", "Ventricular_Amplitude", "Ventricular_Pulse_Width"],
+    "AAI": ["Lower_Rate_Limit", "Upper_Rate_Limit", "Atrial_Amplitude", "Atrial_Pules_Width", "ARP"],
+    "VVI": ["Lower_Rate_Limit", "Upper_Rate_Limit", "Ventricular_Amplitude", "Ventricular_Pulse_Width", "VRP"]
+    }
 
 
 
@@ -53,66 +55,16 @@ mb.menu.add_checkbutton(label = 'About', variable = aboutVar);
 mb.pack();
 
 
-###############################OPEN_SECOND_WINDOW################################################
-
-def open_Second_Window(): #function
-    second = Toplevel(root);
-    second.title("Second Screen");
-    second.geometry("1080x1080");
-
-
-###button_to_open_second window###
-
-open_window_button = Button(root, text = "Open Second Screen", command = open_Second_Window);
-open_window_button.pack(pady=20);
-
-
-
-###############################CLOCK#############################################################
-
-#live clock
-'''
-def time():
-    string = strftime('%H:%M:%S %p');
-    lbl.config(text = string);
-    lbl.after(1000, time);
-
-lbl = Label(root, font =('calibri', 40, 'bold'), background='purple', foreground='white');
-lbl.pack(anchor = 'center')
-time()
-'''
-
-def set_clock():
-    #get user inputs
-    date = date.get();
-    time = time.get();
-
-Label(root, text = "Enter Date (YYYY-MM-DD): ").pack(pady=5)
-date = Entry(root)
-date.pack();
-
-Label(root, text = "Enter Time (HH:MM:SS): ").pack(pady=5);
-time = Entry(root);
-time.pack();
-
-Button(root, text = "Set Clock", command = set_clock).pack(pady=10);
-
-confirmation_label = Label(root, text= "");
-confirmation_label.pack(pady=5);
-
-
-
 ##################################SELECT_MODES#####################################################
 
 
 def select_mode(event):
 
+    global selected_mode
+
     selected_mode = combo_box.get();
     label.config(text = "Selected Mode: " + selected_mode);
     parameters(selected_mode);
-
-
-
 
 
 root.title("Modes");
@@ -125,53 +77,57 @@ combo_box = ttk.Combobox(root, values = Modes, state = 'readonly');
 combo_box.pack(pady = 10);
 
 
-
-
 combo_box.set("Off"); #default state
 combo_box.bind("<<ComboboxSelected>>", select_mode); #allows user to select mode
 
 
+
 #LowerRateLimit
 label_LowerRateLimit = Label(root, text = Programable_Paramters[0]); 
-s_LowerRateLimit = Scale(root, from_=0, to=100, orient = HORIZONTAL); #creates scale from 0 to 100
+s_LowerRateLimit = Scale(root, from_=Lower_Rate_Limit[0], to=Lower_Rate_Limit[2], orient = HORIZONTAL); #creates scale from 0 to 100
+s_LowerRateLimit.set(Lower_Rate_Limit[1]); #sets lower rate limit to nominal value
 
 #UpperRateLimit
 label_UpperRateLimit = Label(root, text = Programable_Paramters[1]); 
-s_UpperRateLimit = Scale(root, from_=0, to=100, orient = HORIZONTAL); #creates scale from 0 to 100
-
+s_UpperRateLimit = Scale(root, from_=Upper_Rate_Limit[0], to=Upper_Rate_Limit[2], orient = HORIZONTAL); #creates scale from 0 to 100
+s_UpperRateLimit.set(Upper_Rate_Limit[1]);
 
 #AtrialAmplitude
 label_AtrialAmplitude = Label(root, text = Programable_Paramters[2]); 
-s_AtrialAmplitude = Scale(root, from_=0, to=100, orient = HORIZONTAL); #creates scale from 0 to 100
+s_AtrialAmplitude = Scale(root, from_=Atrial_Amplitude[0], to=Atrial_Amplitude[2], orient = HORIZONTAL); #creates scale from 0 to 100
+s_AtrialAmplitude.set(Atrial_Amplitude[1]); 
+
 
 #AtrialPulesWidth
 label_AtrialPulesWidth = Label(root, text = Programable_Paramters[3]); 
-s_AtrialPulesWidth = Scale(root, from_=0, to=100, orient = HORIZONTAL); #creates scale from 0 to 100
-
+s_AtrialPulesWidth = Scale(root, from_=Atrial_Pules_Width[0], to=Atrial_Pules_Width[2], orient = HORIZONTAL); #creates scale from 0 to 100
+s_AtrialPulesWidth.set(Atrial_Pules_Width[1]);
 
 #VentricularAmplitude
 label_VentricularAmplitude = Label(root, text = Programable_Paramters[4]); 
-s_VentricularAmplitude = Scale(root, from_=0, to=100, orient = HORIZONTAL); #creates scale from 0 to 100
+s_VentricularAmplitude = Scale(root, from_=Ventricular_Amplitude[0], to=Ventricular_Amplitude[2], orient = HORIZONTAL); #creates scale from 0 to 100
+s_VentricularAmplitude.set(Ventricular_Amplitude[1]);
 
 #VentricularPulseWidth
 label_VentricularPulseWidth = Label(root, text = Programable_Paramters[5]); 
-s_VentricularPulseWidth = Scale(root, from_=0, to=100, orient = HORIZONTAL); #creates scale from 0 to 100
+s_VentricularPulseWidth = Scale(root, from_=Ventricular_Pulse_Width[0], to=Ventricular_Pulse_Width[2], orient = HORIZONTAL); #creates scale from 0 to 100
+s_VentricularPulseWidth.set(Ventricular_Pulse_Width[1]);
 
 #VRP
 label_VRP = Label(root, text = Programable_Paramters[6]); 
-s_VRP = Scale(root, from_=0, to=100, orient = HORIZONTAL); #creates scale from 0 to 100
+s_VRP = Scale(root, from_=VRP[0], to=VRP[2], orient = HORIZONTAL); #creates scale from 0 to 100
+s_VRP.set(VRP[1]);
 
 #ARP
 label_ARP = Label(root, text = Programable_Paramters[7]); 
-s_ARP = Scale(root, from_=0, to=100, orient = HORIZONTAL); #creates scale from 0 to 100
-
+s_ARP = Scale(root, from_=ARP[0], to=ARP[2], orient = HORIZONTAL); #creates scale from 0 to 100
+s_ARP.set(ARP[1]);
 
 
 sList = [s_LowerRateLimit, s_UpperRateLimit,s_AtrialAmplitude,s_AtrialPulesWidth,s_VentricularAmplitude,s_VentricularPulseWidth, s_VRP, s_ARP];
 
 lList = [label_LowerRateLimit, label_UpperRateLimit, label_AtrialAmplitude,label_AtrialPulesWidth,label_VentricularAmplitude, label_VentricularPulseWidth,
          label_VRP, label_ARP];
-
 
 
 
@@ -185,6 +141,70 @@ VVI - Lower Rate Limit, Upper Rate Limit,Ventricular Amplitude, Ventricular Puls
 
 
 
+
+############################Update values######################################
+
+def update_temp_values():
+    Lower_Rate_Limit[3] = s_LowerRateLimit.get()
+    Upper_Rate_Limit[3] = s_UpperRateLimit.get()
+    Atrial_Amplitude[3] = s_AtrialAmplitude.get()
+    Atrial_Pules_Width[3] = s_AtrialPulesWidth.get()
+    Ventricular_Amplitude[3] = s_VentricularAmplitude.get()
+    Ventricular_Pulse_Width[3] = s_VentricularPulseWidth.get()
+    VRP[3] = s_VRP.get()
+    ARP[3] = s_ARP.get()
+
+    root.after(500, update_temp_values); #updates every 0.5s
+
+
+update_temp_values();
+
+
+##################save - temporary to permanent###############################
+
+def save_parameters():
+    Lower_Rate_Limit[4] = Lower_Rate_Limit[3]
+    Upper_Rate_Limit[4] = Upper_Rate_Limit[3]
+    Atrial_Amplitude[4] = Atrial_Amplitude[3]
+    Atrial_Pules_Width[4] = Atrial_Pules_Width[3]
+    
+    Ventricular_Amplitude[4] = Ventricular_Amplitude[3]
+    Ventricular_Pulse_Width[4] = Ventricular_Pulse_Width[3]
+    VRP[4] = VRP[3]
+    ARP[4] = ARP[3]
+
+
+save_button = Button(root, text="Save", command=save_parameters);
+save_button.pack(pady=20);
+
+
+
+def print_parameters():
+
+    parameters = {
+        "Lower_Rate_Limit": Lower_Rate_Limit[4],
+        "Upper_Rate_Limit": Upper_Rate_Limit[4],
+        "Atrial_Amplitude": Atrial_Amplitude[4],
+        "Atrial_Pules_Width": Atrial_Pules_Width[4],
+        "Ventricular_Amplitude": Ventricular_Amplitude[4],
+        "Ventricular_Pulse_Width": Ventricular_Pulse_Width[4],
+        "VRP": VRP[4],
+        "ARP": ARP[4]
+
+    }
+
+    print(f"\nParameters for mode {selected_mode}:")
+    for param in mode_parameters[selected_mode]:
+        print(f"{param}: {parameters[param]}")
+
+Button(root, text="Print Parameters", command=print_parameters).pack()
+
+
+
+
+
+
+    
 
 def parameters(selected_mode):
 
@@ -213,52 +233,6 @@ def parameters(selected_mode):
 
 
             
-    
-
-    
-
-
-
-
-#######################################SLIDER#######################################################
-
-'''
-label_LowerRateLimit = Label(root, text = "Lower Rate Limit").place(x=100, y= 50); 
-s = Scale(root, from_=0, to=100, orient = HORIZONTAL).place(x=100, y= 70); #creates scale from 0 to 100
-#s.pack();
-
-label_UpperRateLimit = Label(root, text = "Upper Rate Limit").place(x=100, y= 120); 
-s = Scale(root, from_=0, to=100, orient = HORIZONTAL).place(x=100, y= 140); #creates scale from 0 to 100
-
-label_AtrialAmplitude = Label(root, text = "Atrial Amplitude").place(x=100, y= 190); 
-s = Scale(root, from_=0, to=100, orient = HORIZONTAL).place(x=100, y= 210); #creates scale from 0 to 100
-
-label_AtrialPulesWidth = Label(root, text = "Atrial Pules Width").place(x=100, y= 260); 
-s = Scale(root, from_=0, to=100, orient = HORIZONTAL).place(x=100, y= 280); #creates scale from 0 to 100
-
-label_VentricularAmplitude = Label(root, text = "Ventricular Amplitude").place(x=100, y= 330); 
-s = Scale(root, from_=0, to=100, orient = HORIZONTAL).place(x=100, y= 350); #creates scale from 0 to 100
-
-label_VentricularPulseWidth = Label(root, text = "Ventricular Pulse Width").place(x=100, y= 400); 
-s = Scale(root, from_=0, to=100, orient = HORIZONTAL).place(x=100, y= 420); #creates scale from 0 to 100
-
-label_VRP = Label(root, text = "VRP").place(x=100, y= 470); 
-s = Scale(root, from_=0, to=100, orient = HORIZONTAL).place(x=100, y= 490); #creates scale from 0 to 100
-
-label_ARP = Label(root, text = "ARP").place(x=100, y= 540); 
-s = Scale(root, from_=0, to=100, orient = HORIZONTAL).place(x=100, y= 560); #creates scale from 0 to 100
-'''
-
-
-#########################################SAVE_BUTTON#####################################################
-
-#right now its just a button that says save but DOESNT DO ANYTHING
-
-save_button = Button(root, text = "Save") #, command = open_Second_Window);
-save_button.pack(pady=20);
-
-
-
 '''
 ###OPENING NEW WINDOW###
 
@@ -277,7 +251,9 @@ button.place(x= 250, y=400) #displays button
 
 second.mainloop(); #runs application
 
+'''
 
+'''
 ###############################OPEN_SECOND_WINDOW################################################
 
 def open_Second_Window(): #function
@@ -293,6 +269,7 @@ open_window_button.pack(pady=20);
 
 '''
 
+'''
 #############################Opens_new_screen_and_closes_old_screen############################################## 
 def successful_login(): #function
     root.destroy(); #close main window
@@ -306,12 +283,58 @@ def successful_login(): #function
     
         
 sucesssful_login_button = Button(root, text = "Sucessful_Login", command = successful_login).place(x=400, y=600);
-
+'''
 
 root.mainloop(); #runs application
 
 
+'''
+###############################OPEN_SECOND_WINDOW################################################
+
+def open_Second_Window(): #function
+    second = Toplevel(root);
+    second.title("Second Screen");
+    second.geometry("1080x1080");
 
 
+###button_to_open_second window###
+
+open_window_button = Button(root, text = "Open Second Screen", command = open_Second_Window);
+open_window_button.pack(pady=20);
+'''
+
+'''
+###############################CLOCK#############################################################
+
+#live clock
+
+def time():
+    string = strftime('%H:%M:%S %p');
+    lbl.config(text = string);
+    lbl.after(1000, time);
+
+lbl = Label(root, font =('calibri', 40, 'bold'), background='purple', foreground='white');
+lbl.pack(anchor = 'center')
+time()
+
+
+def set_clock():
+    #get user inputs
+    date = date.get();
+    time = time.get();
+
+Label(root, text = "Enter Date (YYYY-MM-DD): ").pack(pady=5)
+date = Entry(root)
+date.pack();
+
+Label(root, text = "Enter Time (HH:MM:SS): ").pack(pady=5);
+time = Entry(root);
+time.pack();
+
+Button(root, text = "Set Clock", command = set_clock).pack(pady=10);
+
+confirmation_label = Label(root, text= "");
+confirmation_label.pack(pady=5);
+'''
 
 
