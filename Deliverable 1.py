1d0f6c03cd07c361ec9449e59f4fae85065f92a2
vane1 <89957454+EmilyVan4@users.noreply.github.com>
2025-10-15 13:34:20 -0400

    Update Deliverable 1.py

--- Deliverable 1.py
+++ Deliverable 1.py
@@ -7,8 +7,41 @@ import subprocess
 
 ############################## Variables ##############################
 
-Model_number = "ABCD"
+Device_model = "Pacemaker"
+Device_serial_number = "HOOO25"
 DCM_serial_number = "400325598"
+Model_number = "ABCD"
+Version_number = "1.0"
+
+# programable paramters [1=lower limit, 2=nominal, 3=upper limit, 4=temporary paramater, 5=permanent parameter]
+Lower_Rate_Limit = [30,60,175,60,60] #ppm
+Upper_Rate_Limit = [50,120,175,120,120] #ppm
+Atrial_Amplitude = [0.5,3.5,7.0,3.5,3.5] #V
+Atrial_Pules_Width = [0.05,0.4,1.9,0.4,0.4] #ms
+Ventricular_Amplitude = [0.5,3.5,7.0,3.5,3.5] #V
+Ventricular_Pulse_Width = [0.05,0.4,1.9,0.4,0.4] #ms
+VRP = [150,320,500,320,320] #ms
+ARP = [150,250,500,250,250] #ms
+
+# Constants
+Modes = ["Off", "AOO","VOO", "AAI", "VVI"]
+Programmable_Parameters = ["Lower Rate Limit", "Upper Rate Limit", "Atrial Amplitude","Atrial Pules Width",
+                         "Ventricular Amplitude", "Ventricular Pulse Width", "VRP", "ARP"]
+Mode_Parameters = [[False,False,False,False,False,False,False,False],
+                   [True,True,True,True,False,False,False,False],
+                   [True,True,False,False,True,True,False,False],
+                   [True,True,True,True,False,False,False,True],
+                   [True,True,False,False,True,True,True,False]]
+
+Parameters_Units = [" ppm", " ppm", " V", " ms", " V", " ms", " ms", " ms"]
+
+mode_parameters = {
+    "Off": [" "],
+    "AOO": ["Lower_Rate_Limit", "Upper_Rate_Limit", "Atrial_Amplitude", "Atrial_Pules_Width"],
+    "VOO": ["Lower_Rate_Limit", "Upper_Rate_Limit", "Ventricular_Amplitude", "Ventricular_Pulse_Width"],
+    "AAI": ["Lower_Rate_Limit", "Upper_Rate_Limit", "Atrial_Amplitude", "Atrial_Pules_Width", "ARP"],
+    "VVI": ["Lower_Rate_Limit", "Upper_Rate_Limit", "Ventricular_Amplitude", "Ventricular_Pulse_Width", "VRP"]
+    }
 
 ############################## Setup ##################################
 
@@ -28,7 +61,23 @@ def About():
 
     Model_Number = Label(About_window, text = "Model Number: " + Model_number, font = ('Arial', 14), fg = 'black', bg = "white") #Sets text settings
     Model_Number.place(x=10, y=10) #Displays model number text
-    Software_Revision_Number = Label(About_window, text = "Software Revision Number: Version 1.0", font = ('Arial', 14), fg = 'black', bg = "white") #Sets text settings
+    Software_Revision_Number = Label(About_window, text = "Software Revision Number: Version " + Version_number, font = ('Arial', 14), fg = 'black', bg = "white") #Sets text settings
+    Software_Revision_Number.place(x=10, y=40) #Displays software revision number text
+    DCM_Serial_Number = Label(About_window, text = "DCM Serial Number: " + DCM_serial_number, font = ('Arial', 14), fg = 'black', bg = "white") #Sets text settings
+    DCM_Serial_Number.place(x=10, y=70) #Displays DCM serial number text
+    Institution_Name = Label(About_window, text = "Institution Name: McMaster University", font = ('Arial', 14), fg = 'black', bg = "white") #Sets text settings
+    Institution_Name.place(x=10, y=100) #Displays institution name text
+
+    About_window.mainloop() #Displays the about window
+
+def About_2():
+    About_window = Toplevel() #Initiates about window
+    About_window.geometry("360x180")
+    About_window.title("About") #Sets title
+
+    Model_Number = Label(About_window, text = "Model Number: " + Model_number, font = ('Arial', 14), fg = 'black', bg = "white") #Sets text settings
+    Model_Number.place(x=10, y=10) #Displays model number text
+    Software_Revision_Number = Label(About_window, text = "Software Revision Number: Version " + Version_number, font = ('Arial', 14), fg = 'black', bg = "white") #Sets text settings
     Software_Revision_Number.place(x=10, y=40) #Displays software revision number text
     DCM_Serial_Number = Label(About_window, text = "DCM Serial Number: " + DCM_serial_number, font = ('Arial', 14), fg = 'black', bg = "white") #Sets text settings
     DCM_Serial_Number.place(x=10, y=70) #Displays DCM serial number text
@@ -43,6 +92,7 @@ def Get_input(): #Get username and password and returns it
     return Username_input, Password_input
 
 def Verify_account(username, password): #Check if username and password is correct
+    userdata_exists()
     with open("userdata.json", "r") as file:
         data = json.load(file) 
     for user in data['registered users']:
@@ -52,15 +102,219 @@ def Verify_account(username, password): #Check if username and password is corre
 
 def Successful_login(): #Gives access to my account page
     Window.destroy(); #Close main window
-    My_account = tk.Tk() #Open new window
-    My_account.title("My Account")
-    My_account.geometry("1080x1080")
-    label = tk.Label(My_account, text = "Incorrect username/password", font = ('Arial', 14), fg = 'black', bg = "#CBC3E3") #Sets text settings
-    label.place(x=350, y=425)
+    global root
+    root = tk.Tk() #Open new window
+    root.title("My Account")
+    root.geometry("1080x1080")
 
-    login.mainloop();
+    About_button = Button(root, text = "About", font = ('Arial', 16), fg = 'black', bg = "white") #Sets text settings
+    About_button.place(x=15, y=15) #Displays about button
+    About_button.config(command = About_2) #Sets button to about function
+
+    combo_box_create()
+    scales()
+    parameters(Modes[0])
+    update_temp_values()
+    save_button = Button(root, text="Save", command=save_parameters)
+    save_button.pack(pady=20)
+    Button(root, text="Print Parameters", command=print_parameters).pack()
+    temp_report_button = Button(root, text="Temporary Report", command=lambda:export_report("Temporary"))
+    temp_report_button.pack(pady=20)
+    Bradycardia_report_button = Button(root, text="Bradycardia Report", command=lambda:export_report("Bradycardia"))
+    Bradycardia_report_button.pack(pady=20)
     
+    root.mainloop()
+
+def combo_box_create():
+    root.title("Modes")
+    global label
+    label = tk.Label(root, text = "Selected Mode: ")
+    label.pack(pady = 50)
+
+    global combo_box
+    combo_box = ttk.Combobox(root, values = Modes, state = 'readonly')
+    combo_box.pack(pady = 10)
+
+    combo_box.set("Off") #default state
+    combo_box.bind("<<ComboboxSelected>>", select_mode) #allows user to select mode
+
+def select_mode(event): #selects mode
+
+    global selected_mode
+
+    selected_mode = combo_box.get()
+    label.config(text = "Selected Mode: " + selected_mode)
+    parameters(selected_mode)
+
+def scales():
+    ##Making scales and setting it to default value
+
+    #LowerRateLimit
+    label_LowerRateLimit = Label(root, text = Programmable_Parameters[0]); 
+    s_LowerRateLimit = Scale(root, from_=Lower_Rate_Limit[0], to=Lower_Rate_Limit[2], orient = HORIZONTAL); #creates scale from 0 to 100
+    s_LowerRateLimit.set(Lower_Rate_Limit[1]); #sets lower rate limit to nominal value
+
+    #UpperRateLimit
+    label_UpperRateLimit = Label(root, text = Programmable_Parameters[1]); 
+    s_UpperRateLimit = Scale(root, from_=Upper_Rate_Limit[0], to=Upper_Rate_Limit[2], orient = HORIZONTAL); #creates scale from 0 to 100
+    s_UpperRateLimit.set(Upper_Rate_Limit[1]);
+
+    #AtrialAmplitude
+    label_AtrialAmplitude = Label(root, text = Programmable_Parameters[2]); 
+    s_AtrialAmplitude = Scale(root, from_=Atrial_Amplitude[0], to=Atrial_Amplitude[2], orient = HORIZONTAL); #creates scale from 0 to 100
+    s_AtrialAmplitude.set(Atrial_Amplitude[1]); 
+
+
+    #AtrialPulesWidth
+    label_AtrialPulesWidth = Label(root, text = Programmable_Parameters[3]); 
+    s_AtrialPulesWidth = Scale(root, from_=Atrial_Pules_Width[0], to=Atrial_Pules_Width[2], orient = HORIZONTAL); #creates scale from 0 to 100
+    s_AtrialPulesWidth.set(Atrial_Pules_Width[1]);
+
+    #VentricularAmplitude
+    label_VentricularAmplitude = Label(root, text = Programmable_Parameters[4]); 
+    s_VentricularAmplitude = Scale(root, from_=Ventricular_Amplitude[0], to=Ventricular_Amplitude[2], orient = HORIZONTAL); #creates scale from 0 to 100
+    s_VentricularAmplitude.set(Ventricular_Amplitude[1]);
+
+    #VentricularPulseWidth
+    label_VentricularPulseWidth = Label(root, text = Programmable_Parameters[5]); 
+    s_VentricularPulseWidth = Scale(root, from_=Ventricular_Pulse_Width[0], to=Ventricular_Pulse_Width[2], orient = HORIZONTAL); #creates scale from 0 to 100
+    s_VentricularPulseWidth.set(Ventricular_Pulse_Width[1]);
+
+    #VRP
+    label_VRP = Label(root, text = Programmable_Parameters[6]); 
+    s_VRP = Scale(root, from_=VRP[0], to=VRP[2], orient = HORIZONTAL); #creates scale from 0 to 100
+    s_VRP.set(VRP[1]);
+
+    #ARP
+    label_ARP = Label(root, text = Programmable_Parameters[7]); 
+    s_ARP = Scale(root, from_=ARP[0], to=ARP[2], orient = HORIZONTAL); #creates scale from 0 to 100
+    s_ARP.set(ARP[1]);
+
+    #Puts scales into a list
+    global sList
+    sList = [s_LowerRateLimit, s_UpperRateLimit,s_AtrialAmplitude,s_AtrialPulesWidth,s_VentricularAmplitude,s_VentricularPulseWidth, s_VRP, s_ARP];
+
+    #Puts labels into a list
+    global lList
+    lList = [label_LowerRateLimit, label_UpperRateLimit, label_AtrialAmplitude,label_AtrialPulesWidth,label_VentricularAmplitude, label_VentricularPulseWidth,
+             label_VRP, label_ARP];
+    
+def parameters(selected_mode):
+
+    match selected_mode:
+
+        case "Off":
+            mode = 0
+        case "AOO":
+            mode = 1
+
+        case "VOO":
+            mode = 2
+        case "AAI":
+            mode = 3
+        case "VVI":
+            mode = 4
+
+    for i in range(8):
+        if Mode_Parameters[mode][i]:
+            sList[i].place(x=100, y=70+i*70)
+            lList[i].place(x=100, y= 50+i*70)
+
+        else:
+            sList[i].place_forget()
+            lList[i].place_forget()
+            
+def update_temp_values(): #updates value
+    Lower_Rate_Limit[3] = sList[0].get()
+    Upper_Rate_Limit[3] = sList[1].get()
+    Atrial_Amplitude[3] = sList[2].get()
+    Atrial_Pules_Width[3] = sList[3].get()
+    Ventricular_Amplitude[3] = sList[4].get()
+    Ventricular_Pulse_Width[3] = sList[5].get()
+    VRP[3] = sList[6].get()
+    ARP[3] = sList[7].get()
+
+    root.after(500, update_temp_values); #updates every 0.5s
+
+def save_parameters(): #saves parameters
+    Lower_Rate_Limit[4] = Lower_Rate_Limit[3]
+    Upper_Rate_Limit[4] = Upper_Rate_Limit[3]
+    Atrial_Amplitude[4] = Atrial_Amplitude[3]
+    Atrial_Pules_Width[4] = Atrial_Pules_Width[3]
+    
+    Ventricular_Amplitude[4] = Ventricular_Amplitude[3]
+    Ventricular_Pulse_Width[4] = Ventricular_Pulse_Width[3]
+    VRP[4] = VRP[3]
+    ARP[4] = ARP[3]
+
+def print_parameters():
+
+    parameters = {
+        "Lower_Rate_Limit": Lower_Rate_Limit[4],
+        "Upper_Rate_Limit": Upper_Rate_Limit[4],
+        "Atrial_Amplitude": Atrial_Amplitude[4],
+        "Atrial_Pules_Width": Atrial_Pules_Width[4],
+        "Ventricular_Amplitude": Ventricular_Amplitude[4],
+        "Ventricular_Pulse_Width": Ventricular_Pulse_Width[4],
+        "VRP": VRP[4],
+        "ARP": ARP[4]
+
+    }
+
+    print(f"\nParameters for mode {selected_mode}:")
+    for param in mode_parameters[selected_mode]:
+        print(f"{param}: {parameters[param]}")
+
+# creates a new report of the type given (Bradycardia or Temporary)
+def export_report(type: str):
+
+    # saves current time and labels the output file
+    current_datetime = datetime.now()
+    file_name = type + "_Report-" + current_datetime.strftime("%Y-%m-%d-%H-%M-%S") + ".txt"
+
+    # uses i to output temp or brady paramaters
+    i = 3
+    if type == "Bradycardia":
+        i = 4
+
+    # writes report
+    with open(file_name, 'w') as file:
+        file.write(type + " Parameters Report"+
+                   "\nDate: " + current_datetime.strftime("%Y/%M/%D %H:%M:%S")+
+                   "\nDevice Model: "+ Device_model+
+                    "\nSerial Number: "+ Device_serial_number+
+                    "\nDCM Serial Number: "+ DCM_serial_number+
+                    "\nApplication Model: "+ Model_number+
+                    "\nVersion Number: "+ Version_number+
+                    "\n----------------------------------"
+                    "\n"+ type + " Parameters: "+
+                    "\n"+ "\t" + Programmable_Parameters[0]+ ": "+ str(Lower_Rate_Limit[i]) + Parameters_Units[0]+
+                    "\n"+ "\t" + Programmable_Parameters[1]+ ": "+ str(Upper_Rate_Limit[i]) + Parameters_Units[1]+
+                    "\n"+ "\t" + Programmable_Parameters[2]+ ": "+ str(Atrial_Amplitude[i]) + Parameters_Units[2]+
+                    "\n"+ "\t" + Programmable_Parameters[3]+ ": "+ str(Atrial_Pules_Width[i]) + Parameters_Units[3]+
+                    "\n"+ "\t" + Programmable_Parameters[4]+ ": "+ str(Ventricular_Amplitude[i]) + Parameters_Units[4]+
+                    "\n"+ "\t" + Programmable_Parameters[5]+ ": "+ str(Ventricular_Pulse_Width[i]) + Parameters_Units[5]+
+                    "\n"+ "\t" + Programmable_Parameters[6]+ ": "+ str(VRP[i]) + Parameters_Units[6]+
+                    "\n"+ "\t" + Programmable_Parameters[7]+ ": "+ str(ARP[i]) + Parameters_Units[7]
+      )
+        
+    subprocess.Popen(["notepad.exe",file_name])
+    #return file_name
+
+# check is userdata.json file exists, creates empty one f not
+def userdata_exists():
+    try:
+        with open("userdata.json", 'r') as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        data = {
+            "registered users": []
+            }  
+        with open("userdata.json", 'w') as file:
+            json.dump(data, file, indent=4)
+
 def Add_new_user(username, password): #Checks sign up conditions and if it's all correct then signs up the user
+    userdata_exists()
     with open("userdata.json", "r") as file:
         data = json.load(file) 
     if len(data['registered users']) > 10:
@@ -87,8 +341,8 @@ def Sign_in(): #Gets username and password and verifies if it's correct
     if (Verify == True):
         Successful_login()
     else:
-        Sign_in_label2 = Label(Window, text = "Incorrect username/password", font = ('Arial', 14), fg = 'black', bg = "#CBC3E3") #Sets text settings
-        Sign_in_label2.place(x=395, y=425) #Displays sign in text
+        Sign_in_label = Label(Window, text = "Incorrect username/password", font = ('Arial', 14), fg = 'black', bg = "#CBC3E3") #Sets text settings
+        Sign_in_label.place(x=395, y=425) #Displays sign in text
         Sign_in_label.after(3000, Sign_in_label.destroy) #Removes sign in text after some time
 
 def Sign_up(): #Gets username and password and verifies sign up conditions
@@ -159,6 +413,8 @@ Password_box.place(x=470, y=555); #Display password box
 ############################## Main ##############################
 
 Window.mainloop() #Displays the window
+
+
 
 
 
